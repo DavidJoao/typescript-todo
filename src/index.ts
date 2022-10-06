977ed404db14100eda27c0359078b784e8796dc0
DavidJoao <davidsandoval596@gmail.com>
2022-10-05 21:28:17 -0700

    created form and object for todo list

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from 'uuid'
+// console.log(uuidV4())
 
+const list = document.querySelector<HTMLUListElement>('#list');
+const input = document.querySelector<HTMLInputElement>('#input-field')
+const addButton = document.querySelector('#add-button')
+const form = document.querySelector<HTMLFormElement>('#form')
+
+form?.addEventListener('submit', e => {
+    e.preventDefault()
+
+    if (input?.value == '' || input?.value == null) return
+    
+    const task = {
+        id: uuidV4,
+        title: input.value,
+        completed: false,
+        createdAt: new Date()
+    }
+})
